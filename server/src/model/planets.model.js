7995946d85188121a4e247154c778f20b775e32e
Atulya Kairati <atulyakairati@gmail.com>
2023-01-28 01:42:02 +0530

    load data before server is started

--- server/src/model/planets.model.js
+++ server/src/model/planets.model.js
@@ -1,3 +1,47 @@
-const planets = [];
+const path = require('path')
+const fs = require("fs")
+const { parse } = require("csv-parse")
 
-module.exports = planets
+
+const habitablePlanets = []
+
+
+
+async function loadAllPlanets() {
+    const promise = new Promise((resolve, reject) => {
+        const fileStream = fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'KOI_data.csv'))
+
+        fileStream.pipe(parse({
+            comment: '#',
+            columns: true
+        }))
+
+
+        fileStream.on("data", (data) => {
+            if (isHabitable(data)) {
+                habitablePlanets.push(data)
+            }
+        })
+            .on('end', () => {
+                console.log(`${habitablePlanets.length} habitable planets loaded!`)
+                resolve()
+            })
+            .on('error', (err) => {
+                console.log(err)
+                reject()
+            })
+    })
+
+    return promise
+}
+
+
+
+
+const isHabitable = (planet) =>
+    planet['koi_disposition'] === "CONFIRMED" &&
+    planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11 &&
+    planet['koi_prad'] < 1.6
+
+
+module.exports = { planets: habitablePlanets, loadAllPlanets }
--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,19 @@
 const http = require('http');
 const app = require('./app')
 
+const { loadAllPlanets } = require('./model/planets.model')
+
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-    console.log(`Listening at ${PORT}...`);
-});
+async function startServer(){
+    await loadAllPlanets()
+    // loading data before starting the server 
+
+    server.listen(PORT, () => {
+        console.log(`Listening at ${PORT}...`);
+    });
+}
+
+startServer()
