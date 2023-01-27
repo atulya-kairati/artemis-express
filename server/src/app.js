7e7fead3c3e10ce14099842bb736138f9cdd249e
Atulya Kairati <atulyakairati@gmail.com>
2023-01-28 01:03:11 +0530

    added cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const cors = require('cors');
 const planetsRouter = require('./routesAndControllers/planets/planets.router')
 
+
 const app = express()
+
+app.use(cors({
+    origin: "http://localhost:3000" // url of the client
+}))
+
 app.use(express.json())
 app.use(planetsRouter)
 
